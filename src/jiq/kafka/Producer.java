a49dee30a61b6ad69ecf649bad850d40e0be16a0
吉庆 <694790506@qq.com>
2017-07-28 14:04:08 +0800

    生产者
    更新根据topic启线程

--- src/jiq/kafka/Producer.java
+++ src/jiq/kafka/Producer.java
@@ -24,38 +24,42 @@ import java.util.concurrent.ExecutionException;
 
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import jiq.util.KafkaUtil;
 import jiq.util.LoginUtil;
 import jiq.util.PropertyUtil;
 
-public class ProducerMultThread extends Thread {
-	private static final Logger LOG = LoggerFactory.getLogger(ProducerMultThread.class);
-
-	// 并发的线程数
-	private static final int PRODUCER_THREAD_COUNT = 3;
+public class Producer implements Runnable {
+	private static final Logger LOG = LoggerFactory.getLogger(Producer.class);
 
 	private final String topic;
 	private final Boolean isAsync;
+	private final int partitionNum;
+	private final String bootstrapServers;
 
 	private static final Properties props = new Properties();
 
-	public ProducerMultThread(String produceToTopic, boolean asyncEnable) {
-		topic = produceToTopic;
-		isAsync = asyncEnable;
+	public Producer(String produceToTopic, boolean asyncEnable, int partitionNum, String bootstrapServers) {
+		this.topic = produceToTopic;
+		this.isAsync = asyncEnable;
+		this.partitionNum = partitionNum;
+		this.bootstrapServers = bootstrapServers;
 	}
 
 	/**
 	 * 启动多个线程进行发送
 	 */
+	@Override
 	public void run() {
 		// 指定的线程号，仅用于区分不同的线程
-		for (int threadNum = 0; threadNum < PRODUCER_THREAD_COUNT; threadNum++) {
-			ProducerThread producerThread = new ProducerThread(topic, isAsync, threadNum);
-			producerThread.start();
+		for (int threadNum = 0; threadNum < partitionNum; threadNum++) {
+			ProducerWorker producerThread = new ProducerWorker(topic, isAsync, threadNum, bootstrapServers);
+			new Thread(producerThread).start();
 		}
 	}
 
@@ -74,33 +78,37 @@ public class ProducerMultThread extends Thread {
 	}
 
 	public static void main(String[] args) {
-		LOG.info("Securitymode start.");
+		// 安全模式下启用
 		try {
 			LOG.info("Securitymode start.");
 			securityPrepare();
 		} catch (IOException e) {
 			LOG.error("Security prepare failure.");
-			LOG.error("The IOException occured.", e);
 			return;
 		}
 		LOG.info("Security prepare success.");
-
-		String topic = PropertyUtil.getInstance().getValue("topic", "topic");
+		PropertyUtil property = PropertyUtil.getInstance();
+		String bootstrapServers = property.getValue("bootstrap.servers",
+				"190.15.116.189:21007,190.15.116.196:21007,190.15.116.190:21007");
+		String topic = property.getValue("topic", "topic");
+		String host = bootstrapServers.substring(0, bootstrapServers.indexOf(':'));
+		int partitionNum = KafkaUtil.getPartitionNum(host, 21005, 100000, 64 * 1024, UUID.randomUUID().toString(),
+				topic);
 
 		// 是否使用异步发送模式
 		final boolean asyncEnable = false;
-		ProducerMultThread producerMultThread = new ProducerMultThread(topic, asyncEnable);
-		producerMultThread.start();
+		Producer producer = new Producer(topic, asyncEnable, partitionNum, bootstrapServers);
+		new Thread(producer).start();
 	}
 
 	/**
 	 * 生产者线程类
 	 */
-	private class ProducerThread extends Thread {
-		private int sendThreadId = 0;
-		private String sendTopic = null;
-		private Boolean isAsync;
+	private class ProducerWorker implements Runnable {
 		private final KafkaProducer<Integer, String> producer;
+		private String topic;
+		private Boolean isAsync;
+		private int sendThreadId = 0;
 
 		/**
 		 * 生产者线程类构造方法
@@ -110,79 +118,62 @@ public class ProducerMultThread extends Thread {
 		 * @param threadNum
 		 *            线程号
 		 */
-		public ProducerThread(String topicName, Boolean asyncEnable, int threadNum) {
-			this.sendThreadId = threadNum;
-			this.sendTopic = topicName;
-			this.isAsync = asyncEnable;
-
-			PropertyUtil property = PropertyUtil.getInstance();
-
+		public ProducerWorker(String topicName, Boolean asyncEnable, int threadNum, String bootstrapServers) {
 			// Broker地址列表
-			props.put("bootstrap.servers", property.getValue("bootstrap.servers", "localhost:21007"));
-
+			props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
 			// 客户端ID
-			props.put("client.id", UUID.randomUUID().toString());
-
+			props.put(ProducerConfig.CLIENT_ID_CONFIG, UUID.randomUUID().toString());
 			// Key序列化类
-			props.put("key.serializer",
-					property.getValue("key.serializer", "org.apache.kafka.common.serialization.IntegerSerializer"));
+			props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+					"org.apache.kafka.common.serialization.IntegerSerializer");
 			// Value序列化类
-			props.put("value.serializer",
-					property.getValue("value.serializer", "org.apache.kafka.common.serialization.StringSerializer"));
+			props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+					"org.apache.kafka.common.serialization.StringSerializer");
 			// 协议类型:当前支持配置为SASL_PLAINTEXT或者PLAINTEXT
-			props.put("security.protocol", property.getValue("security.protocol", "SASL_PLAINTEXT"));
-
+			props.put("security.protocol", "SASL_PLAINTEXT");
 			// 服务名
 			props.put("sasl.kerberos.service.name", "kafka");
-
 			// 创建生产者对象
-			producer = new KafkaProducer<Integer, String>(props);
+			producer = new KafkaProducer<>(props);
+			this.sendThreadId = threadNum;
+			this.topic = topicName;
+			this.isAsync = asyncEnable;
 		}
 
 		public void run() {
 			LOG.info("Producer: start.");
 
 			// 用于记录消息条数
-			int messageCount = 1;
+			int messageNo = 1;
 
 			// 每个线程发送的消息条数
-			int messagesPerThread = 5;
-			while (messageCount <= messagesPerThread) {
+			int messagesPerThread = 50;
+			while (messageNo <= messagesPerThread) {
 				// 指定该record发到哪个partition中
 				int partition = sendThreadId;
 				// 时间戳
 				long startTime = System.currentTimeMillis();
 				int key = sendThreadId;
 				// 待发送的消息内容
-				String value = new String("Message_" + sendThreadId + "_" + messageCount);
+				String messageStr = new String("Message_" + sendThreadId + "_" + messageNo);
 
 				// 构造消息记录
 				ProducerRecord<Integer, String> record = new ProducerRecord<Integer, String>(topic, partition,
-						startTime, key, value);
+						startTime, key, messageStr);
 
-				if (isAsync) {
-					// 异步发送
-					producer.send(record, new DemoCallBack(startTime, sendThreadId, value));
-				} else {
+				if (isAsync) { // 异步发送
+					producer.send(record, new DemoCallBack(startTime, sendThreadId, messageStr));
+				} else { // 同步发送
 					try {
-						// 同步发送
 						producer.send(record).get();
-					} catch (InterruptedException ie) {
-						LOG.info("The InterruptedException occured : {}.", ie);
-					} catch (ExecutionException ee) {
-						LOG.info("The ExecutionException occured : {}.", ee);
+					} catch (InterruptedException | ExecutionException e) {
+						e.printStackTrace();
 					}
 				}
 
-				LOG.info("Producer: send " + value + " to " + sendTopic + " with key: " + sendThreadId);
-				messageCount++;
-
-				// 每隔1s，发送1条消息
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				LOG.info("Producer: send " + messageStr + " to " + topic + " partition " + partition + " with key: "
+						+ sendThreadId);
+				++messageNo;
 			}
 
 			try {
@@ -201,9 +192,7 @@ class DemoCallBack implements Callback {
 	private static Logger LOG = LoggerFactory.getLogger(DemoCallBack.class);
 
 	private long startTime;
-
 	private int key;
-
 	private String message;
 
 	public DemoCallBack(long startTime, int key, String message) {
@@ -221,14 +210,15 @@ class DemoCallBack implements Callback {
 	 *            发送异常。如果没有错误发生则为Null。
 	 */
 	@Override
-	public void onCompletion(RecordMetadata metadata, java.lang.Exception exception) {
+	public void onCompletion(RecordMetadata metadata, Exception exception) {
 		long elapsedTime = System.currentTimeMillis() - startTime;
 		if (metadata != null) {
 			LOG.info("message(" + key + ", " + message + ") sent to partition(" + metadata.partition() + "), "
 					+ "offset(" + metadata.offset() + ") in " + elapsedTime + " ms");
 		} else if (exception != null) {
-			LOG.error("The Exception occured.", exception);
+			exception.printStackTrace();
 		}
 
 	}
+
 }
